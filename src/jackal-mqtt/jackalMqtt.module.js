c020dc7b06ee837358e7fbe54a5a19ac25ba4903
Francisco J. Piedrahita <franciscojpiedrahita@gmail.com>
2015-10-26 17:26:52 -0400

    Formated the project with johnpapa/angular-styleguide

--- src/jackal-mqtt/jackalMqtt.module.js
+++ src/jackal-mqtt/jackalMqtt.module.js
@@ -1,21 +1,24 @@
 (function (angular) {
 
-  // Create all modules and define dependencies to make sure they exist
-  // and are loaded in the correct order to satisfy dependency injection
-  // before all nested files are concatenated by Gulp
+    // Create all modules and define dependencies to make sure they exist
+    // and are loaded in the correct order to satisfy dependency injection
+    // before all nested files are concatenated by Gulp
 
-  // Config
-  angular.module('jackalMqtt.config', [])
-      .value('jackalMqtt.config', {
-          debug: true
-      });
+    // Config
+    angular
+        .module('jackalMqtt.config', [])
+        .value('jackalMqtt.config', {
+            debug: true
+        });
 
-  // Modules
-  angular.module('jackalMqtt.services', []);
-  angular.module('jackalMqtt',
-      [
-          'jackalMqtt.config',
-          'jackalMqtt.services'
-      ]);
+    // Modules
+    angular
+        .module('jackalMqtt.services', []);
+
+    angular
+        .module('jackalMqtt', [
+            'jackalMqtt.config',
+            'jackalMqtt.services'
+        ]);
 
 })(angular);
