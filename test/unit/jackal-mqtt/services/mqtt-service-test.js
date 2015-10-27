7e7418e85b90d4f53179e311fc7de117f730bd45
Francisco J. Piedrahita <franciscojpiedrahita@gmail.com>
2015-10-27 14:25:09 -0400

    Added tests for mqtt service

---
+++ test/unit/jackal-mqtt/services/mqtt-service-test.js
@@ -0,0 +1,59 @@
+describe('mqttService', function() {
+
+    // Load the module
+    beforeEach(module('jackalMqtt.services'));
+
+    // Inject the MQTT Service
+    beforeEach(inject(function(_mqttService_){
+        mqttService = _mqttService_;
+    }));
+
+    // Inject the Data Service
+    beforeEach(inject(function(_dataService_){
+        dataService = _dataService_;
+    }));
+
+    describe('addCallback()', function() {
+        var tests = [
+            {
+                data: ['myhome/ground/floor/livingroom/temperature', 'onTemp', 'temp']
+            },
+
+            {
+                data: ['myhome/groundfloor/+/temperature', 'onTemp', 'temp']
+            },
+
+            {
+                data: ['myhome/groundfloor/#', 'onFloorData', 'floor']
+            }
+        ];
+
+        it('MQTT Service ShouldEexist', function() {
+            expect(mqttService).to.be.ok;
+        });
+
+        tests.forEach(function(test){
+            var text = (
+                            'Should be the key: ' + test.data[0]
+                            + '. First position of array: ' + test.data[1]
+                            + '. Second position of array: ' + test.data[2]
+            )
+
+            it(text, function(){
+                var chanel = test.data[0];
+                var callbackName = test.data[1];
+                var dataName = test.data[2];
+
+                mqttService._addCallback(chanel, callbackName, dataName);
+
+                mqttService._callbacks.should.have.property(chanel);
+                mqttService._callbacks[chanel][0].should.equal(callbackName);
+                mqttService._callbacks[chanel][1].should.equal(dataName);
+            });
+        });
+    });
+
+    describe('onMessageArrived', function() {
+
+    });
+});
